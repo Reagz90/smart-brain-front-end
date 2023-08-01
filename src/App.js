9bc9ed146429c1bf287cb107f195de1719bc26ec
Reagz <44364513+Reagz90@users.noreply.github.com>
2023-08-01 21:51:47 +1000

    moving clarifai api call to backend

--- src/App.js
+++ src/App.js
@@ -57,9 +57,6 @@ function App() {
     });
   };
 
-  //Personal access token for Clarifai API
-  const PAT = "876f3f1773944f1da373eb31e465b2fb";
-
   const calculateFaceLocation = (data) => {
     const clarifaiFace =
       data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -86,42 +83,20 @@ function App() {
   const onButtonSubmit = () => {
     setImageUrl(input);
 
-    const IMAGE_URL = input;
-
-    const raw = JSON.stringify({
-      user_app_id: {
-        user_id: "clarifai",
-        app_id: "main",
-      },
-      inputs: [
-        {
-          data: {
-            image: {
-              url: IMAGE_URL,
-            },
-          },
-        },
-      ],
-    });
-
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "x-clarifai-request-id-prefix": "webportal",
-        Authorization: "Key " + PAT,
-      },
-      body: raw,
-    };
-
     // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
     // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
     // this will default to the latest version_id
 
-    fetch(
-      `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
-      requestOptions
-    )
+    fetch("http://localhost:3000/imageurl", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json", // Add the Content-Type header for JSON data
+      },
+      body: JSON.stringify({
+        input: input,
+      }),
+    })
       .then((response) => response.json()) // Parse the response from the Clarifai API call
       .then((data) => {
         // Call calculateFaceLocation with the Clarifai API response
